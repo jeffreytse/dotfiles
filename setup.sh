59bedf236b58003f249c303bfc08ccbe80ec758e
Jeffrey Tse <jeffreytse.mail@gmail.com>
2024-05-26 11:46:28 +0800

    perf: better setup instructions

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+if ! command -v ansible-playbook &> /dev/null; then
+  echo "Command ansible-playbook could not be found!"
+  echo
+  echo "You can install ansible by below commands:"
+  echo
+  echo "  pipx install --include-deps ansible"
+  exit 1
+fi
+
 cd ./ansible && ansible-playbook --ask-become-pass setup.yml
