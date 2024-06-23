987fc566e8938fe0dc15d8d14feda606d638c160
Jeffrey Tse <jeffreytse.mail@gmail.com>
2024-06-23 09:30:58 +0800

    feat: better setup script to cover checking
    This refactor includes the below changes
    
    - Make ansible playbook with the latest collections
    - Better setup script to cover ansible playbook checking
    
    Referenced error message:
    
    Unsupported parameters for (community.general.cargo) module: locked.
    Supported parameters include: version, name, state, path."}

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 if ! command -v ansible-playbook &> /dev/null; then
   echo "Command ansible-playbook could not be found!"
   echo
@@ -9,4 +12,29 @@ if ! command -v ansible-playbook &> /dev/null; then
   exit 1
 fi
 
-cd ./ansible && ansible-playbook --ask-become-pass setup.yml
+ANSIBLE_PLAYBOOK_ARGS=()
+
+function usage() {
+  echo "Usage: setup.sh [OPTIONS]"
+  echo
+  echo "Options:"
+  echo ""
+  echo "--help   Show this message and exit"
+  echo "--check  Run the playbook in check mode (default: false)"
+  exit 0
+}
+
+# idiomatic parameter and option handling in sh
+while test $# -gt 0; do
+  case "$1" in
+      --check) ANSIBLE_PLAYBOOK_ARGS+=("--check");;
+      --help) usage;;
+      --*) echo "bad option $1";;
+        *) usage;;
+  esac
+  shift
+done
+
+cd ./ansible
+ansible-galaxy collection install -r requirements.yml --upgrade
+ansible-playbook --ask-become-pass setup.yml ${ANSIBLE_PLAYBOOK_ARGS[@]}
